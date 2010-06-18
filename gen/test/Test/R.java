a4e374b7043d588017b1caf880846bcaa473a6bc
benjamin.rosenberger@liwest.at <benjamin.rosenberger@liwest.at@4c800e4b-5361-6d29-aff8-0ab0c14742b7>
2010-06-18 10:01:00 +0000

    added a menu with actually no functionallity yet

--- gen/test/Test/R.java
+++ gen/test/Test/R.java
@@ -21,18 +21,19 @@ public final class R {
         public static final int icon=0x7f020003;
     }
     public static final class id {
-        public static final int Button01=0x7f060007;
-        public static final int ImageArrow=0x7f060005;
-        public static final int SpinnerEW=0x7f06000a;
-        public static final int SpinnerSN=0x7f060008;
-        public static final int distance=0x7f060006;
-        public static final int inLatitude=0x7f060009;
-        public static final int inLongitude=0x7f06000b;
-        public static final int input_point=0x7f060001;
-        public static final int latitude=0x7f060003;
-        public static final int longitude=0x7f060004;
-        public static final int table_actposition=0x7f060002;
-        public static final int table_arrow=0x7f060000;
+        public static final int Button01=0x7f070007;
+        public static final int ImageArrow=0x7f070005;
+        public static final int SpinnerEW=0x7f07000a;
+        public static final int SpinnerSN=0x7f070008;
+        public static final int close=0x7f07000c;
+        public static final int distance=0x7f070006;
+        public static final int inLatitude=0x7f070009;
+        public static final int inLongitude=0x7f07000b;
+        public static final int input_point=0x7f070001;
+        public static final int latitude=0x7f070003;
+        public static final int longitude=0x7f070004;
+        public static final int table_actposition=0x7f070002;
+        public static final int table_arrow=0x7f070000;
     }
     public static final class layout {
         public static final int main=0x7f030000;
@@ -41,10 +42,14 @@ public final class R {
         public static final int table_inputs=0x7f030003;
         public static final int testlayout=0x7f030004;
     }
+    public static final class menu {
+        public static final int options_menu=0x7f060000;
+    }
     public static final class string {
-        public static final int Button01=0x7f040003;
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
-        public static final int hello2=0x7f040002;
+        public static final int Button01=0x7f040004;
+        public static final int app_name=0x7f040002;
+        public static final int close=0x7f040000;
+        public static final int hello=0x7f040001;
+        public static final int hello2=0x7f040003;
     }
 }
